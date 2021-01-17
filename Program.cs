54476ee9c09e4c607d53c0fa210a519c2b5c3cee
darley <darleycsmonte@fac.pe.senac.br>
2021-01-17 15:38:20 -0300

    Fatorial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1; 
+            int numero;
+
+            Console.WriteLine("Digite um número para calcular o fatorial: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >=1 ){
+                fatorial = fatorial * numero;
+                numero-=1;
+            }
+            Console.WriteLine($"O fatorial  é = {fatorial}");
+
+            
+        }
+    }
+}
